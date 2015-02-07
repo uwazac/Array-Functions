7f7a27b39c5c233a4f6d90e37122ee027128e21e
uwazac <geoffreystephens0@gmail.com>
2015-02-06 18:08:54 -0700

    updates version

--- R/totalMerge.R
+++ R/totalMerge.R
@@ -1,5 +1,4 @@
-totalMerge <-
-function(ArrayData, anotfile="~/Documents/Array Analyses/U133plus2annotation.csv", commonCol="ProbeID")
+totalMerge <-function(ArrayData, anotfile="~/Documents/Array Analyses/U133plus2annotation.csv", commonCol="ProbeID")
 {
   #append gene names to files containing only affy probe IDs
   annotated<-read.csv(anotfile)
